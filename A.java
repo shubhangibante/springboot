7c413834720a46e5b0c5842993ca0a857ecb195d
shubhangibante <shubhangibante20@gmail.com>
2022-02-16 20:15:56 +0530

    I m commiting

---
+++ A.java
@@ -0,0 +1,15 @@
+public class A
+{ 
+  public static void main(String[] args)
+{
+   int age=Integer.parseInt(args[0]);
+if(age>=18)
+{
+  System.out.println("Valid..");
+}
+else
+{
+ System.out.println("InValid..");
+}
+}
+}
